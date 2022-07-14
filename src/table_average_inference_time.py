ced17b066628705a98d080952b8980e4d186ffbd
Olivier Brochu <olivier_brochu@hotmail.com>
2022-07-14 17:40:34 +0300

    Added script to print a table of the average inference time

---
+++ src/table_average_inference_time.py
@@ -0,0 +1,16 @@
+import argparse
+import pandas
+
+from preprocess import preprocess
+
+# Parse the arguments of the command line
+parser = argparse.ArgumentParser(description="Produces a table of the average inference time according to the batch size.")
+
+parser.add_argument("results", help="The results of the processing")
+
+args = parser.parse_args()
+
+results = pandas.read_csv(args.results)
+results = preprocess(results)
+
+print(results["average_time"].to_string(index=False))
